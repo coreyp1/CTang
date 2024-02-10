2ca79ba2a0a40ae7a1c030093547b67b673ffc1f
coreyp_1 <pennycuff.c@gmail.com>
2024-02-10 15:01:06 -0600

    clean up compile and program creation flags as well as add missing documentation

--- include/tang/program.h
+++ include/tang/program.h
@@ -21,38 +21,200 @@ extern "C" {
 
 typedef struct GTA_Context GTA_Context;
 
+/**
+ * The flags for a program.
+ *
+ * @see GTA_PROGRAM_FLAG_DEFAULT
+ * @see GTA_PROGRAM_FLAG_DEBUG
+ * @see GTA_PROGRAM_FLAG_DUPLICATE_CODE
+ * @see GTA_PROGRAM_FLAG_DISABLE_BYTECODE
+ * @see GTA_PROGRAM_FLAG_DISABLE_BINARY
+ */
+typedef uint32_t GTA_Program_Flags;
+
+/**
+ * Use default flags for the program.
+ *
+ * The default flag set is to have no flags set.
+ *
+ * @see GTA_Program_Flags
+ * @see gta_program_create()
+ */
+#define GTA_PROGRAM_FLAG_DEFAULT 0
+
+/**
+ * Enable debug mode for the program.
+ *
+ * @see GTA_Program_Flags
+ * @see gta_program_create()
+ */
+#define GTA_PROGRAM_FLAG_DEBUG 1
+
+/**
+ * When creating the program, do not adopt the code.  Rather, make a copy of
+ * it.
+ *
+ * @see GTA_Program_Flags
+ * @see gta_program_create()
+ */
+#define GTA_PROGRAM_FLAG_DUPLICATE_CODE 2
+
+/**
+ * Disable bytecode generation for the program.
+ *
+ * @see GTA_Program_Flags
+ * @see gta_program_create()
+ */
+#define GTA_PROGRAM_FLAG_DISABLE_BYTECODE 4
+
+/**
+ * Disable binary generation for the program.
+ *
+ * @see GTA_Program_Flags
+ * @see gta_program_create()
+ */
+#define GTA_PROGRAM_FLAG_DISABLE_BINARY 8
+
+/**
+ * Holds the metadata for a program.
+ */
 typedef struct GTA_Program {
+  /**
+   * The code that the program was created from.
+   */
   const char * code;
+  /**
+   * The AST for the program, if parsing was successful.
+   */
   GTA_Ast_Node * ast;
+  /**
+   * The bytecode for the program, if it was generated.
+   */
   GTA_Bytecode_Vector * bytecode;
+  /**
+   * The binary for the program, if it was generated.
+   */
   void * binary;
-  uint64_t flags;
+  /**
+   * The flags for the program.
+   */
+  GTA_Program_Flags flags;
 } GTA_Program;
 
-#define GTA_PROGRAM_FLAG_DEFAULT 0
-#define GTA_PROGRAM_FLAG_DEBUG 1
-#define GTA_PROGRAM_FLAG_DUPLICATE_CODE 2
-#define GTA_PROGRAM_FLAG_FORCE_BYTECODE 4
-#define GTA_PROGRAM_FLAG_FORCE_BINARY 8
-
+/**
+ * Create a new program from the given code.
+ *
+ * Default flags are used based on environment variables.  If no environment
+ * variables are set, then a default configuration will be chosen.
+ *
+ * | Environment Variable | Default Value | Description |
+ * | -------------------- | ------------- | ----------- |
+ * | TANG_DEBUG |  | If set, then the program will be created with the debug flag enabled. |
+ * | TANG_DISABLE_BYTECODE |  | If set, then programs will not be compiled to bytecode. |
+ * | TANG_DISABLE_BINARY |  | If set, then programs will not be compiled to binary. |
+ *
+ * @param code The code to create the program from.
+ * @return The new program or null if the program could not be created.
+ */
 GTA_Program * gta_program_create(const char * code);
 
+/**
+ * Create a new program from the given code in the memory location provided.
+ *
+ * Default flags are used based on environment variables.  For more details,
+ * see gta_program_create().
+ *
+ * A program created in place must only be destroyed with
+ * gta_program_destroy_in_place().
+ *
+ * @see gta_program_destroy_in_place()
+ *
+ * @param program The memory location to create the program in.
+ * @param code The code to create the program from.
+ * @return True if the program was created successfully, false otherwise.
+ */
 bool gta_program_create_in_place(GTA_Program * program, const char * code);
 
-GTA_Program * gta_program_create_with_flags(const char * code, uint64_t flags);
+/**
+ * Create a new program from the given code with the given flags.
+ *
+ * @param code The code to create the program from.
+ * @param flags The flags to create the program with.
+ * @return The new program or null if the program could not be created.
+ */
+GTA_Program * gta_program_create_with_flags(const char * code, GTA_Program_Flags flags);
 
-bool gta_program_create_in_place_with_flags(GTA_Program * program, const char * code, uint64_t flags);
+/**
+ * Create a new program from the given code with the given flags in the memory
+ * location provided.
+ *
+ * A program created in place must only be destroyed with
+ * gta_program_destroy_in_place().
+ *
+ * @see gta_program_destroy_in_place()
+ *
+ * @param program The memory location to create the program in.
+ * @param code The code to create the program from.
+ * @param flags The flags to create the program with.
+ * @return True if the program was created successfully, false otherwise.
+ */
+bool gta_program_create_in_place_with_flags(GTA_Program * program, const char * code, GTA_Program_Flags flags);
 
+/**
+ * Destroy the given program.
+ *
+ * @param program The program to destroy.
+*/
 void gta_program_destroy(GTA_Program * program);
 
+/**
+ * Destroy the given program, except for the program container.
+ *
+ * Use this only if you have allocated the program container yourself.
+ *
+ * @see gta_program_create_in_place()
+ * @see gta_program_create_in_place_with_flags()
+ *
+ * @param program The program to destroy.
+ */
 void gta_program_destroy_in_place(GTA_Program * program);
 
+/**
+ * Execute the given program with the given context.
+ *
+ * If the program was compiled to binary, then the binary will be executed.
+ * Otherwise, if the program was compiled to bytecode then the bytecode will be
+ * executed.  If the program has neither, then the function will return false.
+ *
+ * @param program The program to execute.
+ * @param context The context to execute the program with.
+ * @return True if the program executed successfully, false otherwise.
+ */
 bool gta_program_execute(GTA_Program * program, GTA_Context * context);
 
+/**
+ * Execute the given program with the given context using the bytecode.
+ *
+ * @param program The program to execute.
+ * @param context The context to execute the program with.
+ * @return True if the program executed successfully, false otherwise.
+ */
 bool gta_program_execute_bytecode(GTA_Program * program, GTA_Context * context);
 
+/**
+ * Execute the given program with the given context using the binary.
+ *
+ * @param program The program to execute.
+ * @param context The context to execute the program with.
+ * @return True if the program executed successfully, false otherwise.
+ */
 bool gta_program_execute_binary(GTA_Program * program, GTA_Context * context);
 
+/**
+ * Print the bytecode for the given program.
+ *
+ * @param program The program to print the bytecode for.
+ */
 void gta_program_bytecode_print(GTA_Program * program);
 
 #ifdef __cplusplus
--- src/program.c
+++ src/program.c
@@ -1,4 +1,5 @@
 
+#include <stdlib.h>
 #include <cutil/memory.h>
 #include "tang/program.h"
 #include "tang/tangLanguage.h"
@@ -6,68 +7,7 @@
 #include "tang/astNodeParseError.h"
 #include "tang/virtualMachine.h"
 
-GTA_Program * gta_program_create(const char * code) {
-  GTA_Program * program = gcu_malloc(sizeof(GTA_Program));
-  if (!program) {
-    return 0;
-  }
-
-  if (!gta_program_create_in_place(program, code)) {
-    gcu_free(program);
-    return 0;
-  }
-  return program;
-}
-
-bool gta_program_create_in_place(GTA_Program * program, const char * code) {
-  return gta_program_create_in_place_with_flags(program, code, GTA_PROGRAM_FLAG_DEFAULT);
-}
-
-GTA_Program * gta_program_create_with_flags(const char * code, uint64_t flags) {
-  GTA_Program * program = gcu_malloc(sizeof(GTA_Program));
-  if (!program) {
-    return 0;
-  }
-
-  if (!gta_program_create_in_place_with_flags(program, code, flags)) {
-    gcu_free(program);
-    return 0;
-  }
-  return program;
-}
-
-bool gta_program_create_in_place_with_flags(GTA_Program * program, const char * code, uint64_t flags) {
-  // Initialize the program data structure.
-  *program = (GTA_Program) {
-    .code = code,
-    .ast = 0,
-    .bytecode = 0,
-    .binary = 0,
-    .flags = flags,
-  };
-
-  // Either parse the code into an AST or create a null AST.
-  program->ast = gta_tang_parse(code);
-  if (!program->ast) {
-    program->ast = gta_ast_node_create((GTA_PARSER_LTYPE) {
-      .first_line = 0,
-      .first_column = 0,
-      .last_line = 0,
-      .last_column = 0,
-    });
-  }
-  else if (GTA_AST_IS_PARSE_ERROR(program->ast)) {
-    // If the AST is a parse error, then the program is not valid.
-    gta_ast_node_destroy(program->ast);
-    program->ast = 0;
-    return false;
-  }
-  if (!program->ast) {
-    // If there is no AST, then there is no program.
-    return false;
-  }
-
-  // First, compile the AST into bytecode.
+static void gta_program_compile_bytecode(GTA_Program * program) {
   GCU_Vector64 * bytecode = gcu_vector64_create(0);
   bool no_memory_error = true;
   if (bytecode) {
@@ -107,9 +47,92 @@ bool gta_program_create_in_place_with_flags(GTA_Program * program, const char * 
       }
     }
   }
+}
 
-  // Second, try to compile the AST to a binary.
-  // TODO: Implement this.
+static void gta_program_compile_binary(GTA_Program * program) {
+  (void) program;
+}
+
+GTA_Program * gta_program_create(const char * code) {
+  GTA_Program * program = gcu_malloc(sizeof(GTA_Program));
+  if (!program) {
+    return 0;
+  }
+
+  if (!gta_program_create_in_place(program, code)) {
+    gcu_free(program);
+    return 0;
+  }
+  return program;
+}
+
+bool gta_program_create_in_place(GTA_Program * program, const char * code) {
+  // Initialize flags based on environment variables.
+  GTA_Program_Flags flags = GTA_PROGRAM_FLAG_DEFAULT;
+  if (getenv("TANG_DEBUG")) {
+    flags |= GTA_PROGRAM_FLAG_DEBUG;
+  }
+  if (getenv("TANG_DISABLE_BYTECODE")) {
+    flags |= GTA_PROGRAM_FLAG_DISABLE_BYTECODE;
+  }
+  if (getenv("TANG_DISABLE_BINARY")) {
+    flags |= GTA_PROGRAM_FLAG_DISABLE_BINARY;
+  }
+  return gta_program_create_in_place_with_flags(program, code, flags);
+}
+
+GTA_Program * gta_program_create_with_flags(const char * code, GTA_Program_Flags flags) {
+  GTA_Program * program = gcu_malloc(sizeof(GTA_Program));
+  if (!program) {
+    return 0;
+  }
+
+  if (!gta_program_create_in_place_with_flags(program, code, flags)) {
+    gcu_free(program);
+    return 0;
+  }
+  return program;
+}
+
+bool gta_program_create_in_place_with_flags(GTA_Program * program, const char * code, GTA_Program_Flags flags) {
+  // Initialize the program data structure.
+  *program = (GTA_Program) {
+    .code = code,
+    .ast = 0,
+    .bytecode = 0,
+    .binary = 0,
+    .flags = flags,
+  };
+
+  // Either parse the code into an AST or create a null AST.
+  program->ast = gta_tang_parse(code);
+  if (!program->ast) {
+    program->ast = gta_ast_node_create((GTA_PARSER_LTYPE) {
+      .first_line = 0,
+      .first_column = 0,
+      .last_line = 0,
+      .last_column = 0,
+    });
+  }
+  else if (GTA_AST_IS_PARSE_ERROR(program->ast)) {
+    // If the AST is a parse error, then the program is not valid.
+    gta_ast_node_destroy(program->ast);
+    program->ast = 0;
+    return false;
+  }
+  if (!program->ast) {
+    // If there is no AST, then there is no program.
+    return false;
+  }
+
+  // If the program can compile to binary, then there is no need to compile to
+  // bytecode.
+  if (!(flags & GTA_PROGRAM_FLAG_DISABLE_BINARY)) {
+    gta_program_compile_binary(program);
+  }
+  if (!program->binary && !(flags & GTA_PROGRAM_FLAG_DISABLE_BYTECODE)) {
+    gta_program_compile_bytecode(program);
+  }
 
   return program->bytecode || program->binary;
 }
@@ -135,17 +158,7 @@ bool gta_program_execute(GTA_Program * program, GTA_Context * context) {
   if (!gta_context_create_in_place(context, program)) {
     return false;
   }
-  if (program->flags & GTA_PROGRAM_FLAG_FORCE_BINARY) {
-    if (!program->binary) {
-      return false;
-    }
-    return gta_program_execute_binary(program, context);
-  } else if (program->flags & GTA_PROGRAM_FLAG_FORCE_BYTECODE) {
-    if (!program->bytecode) {
-      return false;
-    }
-    return gta_program_execute_bytecode(program, context);
-  } else if (program->binary) {
+  if (program->binary) {
     return gta_program_execute_binary(program, context);
   } else if (program->bytecode) {
     return gta_program_execute_bytecode(program, context);
