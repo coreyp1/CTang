95d4aef77753e117ab03a9094c559ba503148d85
Corey Pennycuff <pennycuff.c@gmail.com>
2024-05-26 16:13:54 -0500

    AST Node String now uses binary write functions

--- src/ast/astNodeString.c
+++ src/ast/astNodeString.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <cutil/memory.h>
 #include <tang/ast/astNodeString.h>
+#include <tang/program/binary.h>
 #include <tang/computedValue/computedValueString.h>
 
 GTA_Ast_Node_VTable gta_ast_node_string_vtable = {
@@ -65,49 +66,32 @@ bool gta_ast_node_string_compile_to_bytecode(GTA_Ast_Node * self, GTA_Bytecode_C
 }
 
 
-bool gta_ast_node_string_compile_to_binary(GTA_Ast_Node * self, GTA_Binary_Compiler_Context * context) {
-  (void)self;
-  (void)context;
+bool gta_ast_node_string_compile_to_binary(GTA_Ast_Node * self, GTA_MAYBE_UNUSED(GTA_Binary_Compiler_Context * context)) {
   GTA_Ast_Node_String * string = (GTA_Ast_Node_String *)self;
   GCU_Vector8 * v = context->binary_vector;
-  if (!gcu_vector8_reserve(v, v->count + 44)) {
-    return false;
-  }
-#if GTA_X86_64
-  // 64-bit x86
+
+  return true
   // Set up for a function call.
   //   push rbp
   //   mov rbp, rsp
   //   and rsp, 0xFFFFFFFFFFFFFFF0
-  GTA_BINARY_WRITE1(v, 0x55);
-  GTA_BINARY_WRITE3(v, 0x48, 0x89, 0xE5);
-  GTA_BINARY_WRITE4(v, 0x48, 0x83, 0xE4, 0xF0);
-  // context is in r15.
+    && gta_push_reg__x86_64(v, GTA_REG_RBP)
+    && gta_mov_reg_reg__x86_64(v, GTA_REG_RBP, GTA_REG_RSP)
+    && gta_and_reg_imm__x86_64(v, GTA_REG_RSP, 0xFFFFFFF0)
   // gta_computed_value_string_create(&string->string, 0, context):
   //   mov rdi, string->string
-  GTA_BINARY_WRITE2(v, 0x48, 0xBF);
-  GTA_BINARY_WRITE8(v, 0xDE, 0xAD, 0xBE, 0xEF, 0xDE, 0xAD, 0xBE, 0xEF);
-  memcpy(&v->data[v->count - 8], &string->string, 8);
   //   mov rsi, 0x0
-  GTA_BINARY_WRITE2(v, 0x48, 0xC7);
-  GTA_BINARY_WRITE5(v, 0xC6, 0x00, 0x00, 0x00, 0x00);
   //   mov rdx, r15
-  GTA_BINARY_WRITE3(v, 0x4C, 0x89, 0xFA);
+    && gta_mov_reg_imm__x86_64(v, GTA_REG_RDI, (int64_t)string->string)
+    && gta_mov_reg_imm__x86_64(v, GTA_REG_RSI, 0)
+    && gta_mov_reg_reg__x86_64(v, GTA_REG_RDX, GTA_REG_R15)
   //   mov rax, gta_computed_value_string_create
-  GTA_BINARY_WRITE2(v, 0x48, 0xB8);
-  GTA_BINARY_WRITE8(v, 0xDE, 0xAD, 0xBE, 0xEF, 0xDE, 0xAD, 0xBE, 0xEF);
-  GTA_UInteger fp = GTA_JIT_FUNCTION_CONVERTER(gta_computed_value_string_create);
-  memcpy(&v->data[v->count - 8], &fp, 8);
+    && gta_mov_reg_imm__x86_64(v, GTA_REG_RAX, (int64_t)gta_computed_value_string_create)
   //   call rax
-  GTA_BINARY_WRITE2(v, 0xFF, 0xD0);
+    && gta_call_reg__x86_64(v, GTA_REG_RAX)
   // Tear down the function call.
   //   mov rsp, rbp
   //   pop rbp
-  GTA_BINARY_WRITE3(v, 0x48, 0x89, 0xEC);
-  GTA_BINARY_WRITE1(v, 0x5D);
-
-  return true;
-#endif
-
-  return false;
+    && gta_mov_reg_reg__x86_64(v, GTA_REG_RSP, GTA_REG_RBP)
+    && gta_pop_reg__x86_64(v, GTA_REG_RBP);
 }
