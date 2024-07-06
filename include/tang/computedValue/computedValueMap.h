8659d85772a3cd34aaa238e17f9033fe5c7df0cf
Corey Pennycuff <pennycuff.c@gmail.com>
2024-07-06 00:13:33 -0500

    Forgot to add Computed Value Map source files.

---
+++ include/tang/computedValue/computedValueMap.h
@@ -0,0 +1,143 @@
+/**
+ * @file
+ */
+
+#ifndef TANG_COMPUTED_VALUE_MAP_H
+#define TANG_COMPUTED_VALUE_MAP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+#include <tang/computedValue/computedValue.h>
+
+/**
+ * The VTable for the ComputedValueMap.
+ */
+extern GTA_Computed_Value_VTable gta_computed_value_map_vtable;
+
+/**
+ * A Computed Value Error for when a requested key is not found in the map.
+ */
+extern GTA_Computed_Value * gta_computed_value_error_map_key_not_found;
+
+/**
+ * A Computed Value Error for when a supplied key is not a string.
+ */
+extern GTA_Computed_Value * gta_computed_value_error_map_key_not_string;
+
+/**
+ * An object that maps identifiers to computed values.
+ *
+ * Internally, the map's key/value pairs are stored in a pair of hash tables, a
+ * key hash and a value hash.  The two hash tables are kept in sync with each
+ * other.  They use the same hash value (from the key-string) to refer to both
+ * the key and the value.  The reason for this is so that the map can be
+ * iterated over and that the actual key string value is recoverable.
+ */
+typedef struct GTA_Computed_Value_Map {
+  /**
+   * The base class for the computed value.
+   */
+  GTA_Computed_Value base;
+  /**
+   * The hash table for the keys.
+   */
+  GTA_HashX * key_hash;
+  /**
+   * The hash table for the values.
+   */
+  GTA_HashX * value_hash;
+} GTA_Computed_Value_Map;
+
+/**
+ * Create a new computed value for a map container.
+ *
+ * @param size The expected size of the map.
+ * @param context The execution context to create the value in.
+ * @return The new computed value for the map.
+ */
+GTA_NO_DISCARD GTA_Computed_Value * GTA_CALL gta_computed_value_map_create(size_t size, GTA_Execution_Context * context);
+
+/**
+ * Create a new computed value for a map in place.
+ *
+ * @param self The memory address of the computed value.
+ * @param size The expected size of the map.
+ * @param context The execution context to create the value in.
+ * @return True if the operation was successful, false otherwise.
+ */
+bool GTA_CALL gta_computed_value_map_create_in_place(GTA_Computed_Value_Map * self, size_t size, GTA_Execution_Context * context);
+
+/**
+ * Destroy a computed value for a map.
+ *
+ * @see gta_computed_value_destroy
+ *
+ * @param self The computed value for the map.
+ */
+void GTA_CALL gta_computed_value_map_destroy(GTA_Computed_Value * self);
+
+/**
+ * Destroy a computed value for a map in place.
+ *
+ * @see gta_computed_value_destroy_in_place
+ *
+ * @param self The computed value for the map.
+ */
+void GTA_CALL gta_computed_value_map_destroy_in_place(GTA_Computed_Value * self);
+
+/**
+ * Deep copy a computed value for a map.
+ *
+ * @see gta_computed_value_deep_copy
+ *
+ * @param value The computed value to be copied.
+ * @param context The execution context of the program.
+ * @return The deep copy of the ComputedValueMap or NULL if an error occurred.
+ */
+GTA_NO_DISCARD GTA_Computed_Value * GTA_CALL gta_computed_value_map_deep_copy(GTA_Computed_Value * value, GTA_Execution_Context * context);
+
+/**
+ * Get a string representation of the computed value for a map.
+ *
+ * The caller is responsible for freeing the returned string.
+ *
+ * @see gta_computed_value_to_string
+ *
+ * @param self The computed value for the map.
+ * @return The string representation of the computed value for the map.
+ */
+GTA_NO_DISCARD char * GTA_CALL gta_computed_value_map_to_string(GTA_Computed_Value * self);
+
+/**
+ * Get the computed value for a key in the map.
+ * 
+ * @param self The map on which to operate
+ * @param key A string key to look up in the map
+ * @return The computed value for the key or NULL if the key is not found.
+ */
+GTA_NO_DISCARD GTA_Computed_Value * GTA_CALL gta_computed_value_map_get_from_cstring(GTA_Computed_Value_Map * self, const char * key);
+
+/**
+ * Add a computed value to the map using the provided key.
+ *
+ * This function will be a helper function for the Map's index assign function.
+ * An index assign leaves the rhs value on the top of the stack after the
+ * insertion and assignment, so ultimately the value of the key/value pair is
+ * what should be returned.
+ *
+ * The map will take ownership of the key and value.
+ *
+ * @param self The map on which to operate.
+ * @param key The key to use for the insertion.
+ * @param value The value to insert into the map.
+ * @return The value of the key/value pair or NULL if an error occurred.
+ */
+GTA_NO_DISCARD GTA_Computed_Value * GTA_CALL gta_computed_value_map_set_key_val(GTA_Computed_Value_Map * self, GTA_Computed_Value * key, GTA_Computed_Value * value);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // TANG_COMPUTED_VALUE_MAP_H
---
+++ src/computedValue/computedValueMap.c
@@ -0,0 +1,278 @@
+#include <string.h>
+#include <stdio.h>
+#include <cutil/hash.h>
+#include <cutil/memory.h>
+#include <tang/computedValue/computedValueError.h>
+#include <tang/computedValue/computedValueMap.h>
+#include <tang/computedValue/computedValueString.h>
+#include <tang/program/executionContext.h>
+
+
+GTA_Computed_Value_VTable gta_computed_value_map_vtable = {
+  .name = "Iterator",
+  .destroy = gta_computed_value_map_destroy,
+  .destroy_in_place = gta_computed_value_map_destroy_in_place,
+  .deep_copy = gta_computed_value_map_deep_copy,
+  .to_string = gta_computed_value_null_to_string,
+  .print = gta_computed_value_generic_print_from_to_string,
+  .assign_index = gta_computed_value_assign_index_not_supported,
+  .add = gta_computed_value_add_not_supported,
+  .subtract = gta_computed_value_subtract_not_supported,
+  .multiply = gta_computed_value_multiply_not_supported,
+  .divide = gta_computed_value_divide_not_supported,
+  .modulo = gta_computed_value_modulo_not_supported,
+  .negative = gta_computed_value_negative_not_supported,
+  .less_than = gta_computed_value_less_than_not_supported,
+  .less_than_equal = gta_computed_value_less_than_equal_not_supported,
+  .greater_than = gta_computed_value_greater_than_not_supported,
+  .greater_than_equal = gta_computed_value_greater_than_equal_not_supported,
+  .equal = gta_computed_value_equal_not_supported,
+  .not_equal = gta_computed_value_not_equal_not_supported,
+  .period = gta_computed_value_period_not_supported,
+  .index = gta_computed_value_index_not_supported,
+  .slice = gta_computed_value_slice_not_supported,
+  .iterator_get = gta_computed_value_iterator_get_not_supported,
+  .iterator_next = gta_computed_value_iterator_next_not_supported,
+  .cast = gta_computed_value_cast_not_supported,
+  .call = gta_computed_value_call_not_supported,
+};
+
+
+static GTA_Computed_Value_Error gta_computed_value_error_map_key_not_found_singleton = {
+  .base = {
+    .vtable = &gta_computed_value_error_vtable,
+    .context = 0,
+    .is_true = false,
+    .is_error = true,
+    .is_temporary = false,
+    .requires_deep_copy = false,
+    .is_singleton = true,
+    .is_a_reference = false,
+  },
+  .message = "Map Key Not Found",
+};
+
+GTA_Computed_Value * gta_computed_value_error_map_key_not_found = (GTA_Computed_Value *)&gta_computed_value_error_map_key_not_found_singleton;
+
+
+static GTA_Computed_Value_Error gta_computed_value_error_map_key_not_string_singleton = {
+  .base = {
+    .vtable = &gta_computed_value_error_vtable,
+    .context = 0,
+    .is_true = false,
+    .is_error = true,
+    .is_temporary = false,
+    .requires_deep_copy = false,
+    .is_singleton = true,
+    .is_a_reference = false,
+  },
+  .message = "Map Key Is Not A String",
+};
+
+GTA_Computed_Value * gta_computed_value_error_map_key_not_string = (GTA_Computed_Value *)&gta_computed_value_error_map_key_not_string_singleton;
+
+GTA_Computed_Value * GTA_CALL gta_computed_value_map_create(size_t size, GTA_Execution_Context * context) {
+  GTA_Computed_Value_Map * self = gcu_malloc(sizeof(GTA_Computed_Value_Map));
+  if (!self) {
+    return NULL;
+  }
+  if (!gta_computed_value_map_create_in_place(self, size, context)) {
+    gcu_free(self);
+    return NULL;
+  }
+  // Attempt to add the pointer to the context's garbage collection list.
+  if (!GTA_VECTORX_APPEND(context->garbage_collection, GTA_TYPEX_MAKE_P(self))) {
+    gta_computed_value_map_destroy_in_place(&self->base);
+    return gta_computed_value_error_out_of_memory;
+  }
+  return (GTA_Computed_Value *)self;
+}
+
+
+bool GTA_CALL gta_computed_value_map_create_in_place(GTA_Computed_Value_Map * self, size_t size, GTA_Execution_Context * context) {
+  GTA_HashX * key_hash = GTA_HASHX_CREATE(size);
+  if (!key_hash) {
+    goto KEY_HASH_CREATE_FAILED;
+  }
+  GTA_HashX * value_hash = GTA_HASHX_CREATE(size);
+  if (!value_hash) {
+    goto VALUE_HASH_CREATE_FAILED;
+  }
+
+  *self = (GTA_Computed_Value_Map){
+    .base = {
+      .vtable = &gta_computed_value_map_vtable,
+      .context = context,
+      .is_true = false,
+      .is_error = false,
+      .is_temporary = false,
+      .requires_deep_copy = false,
+      .is_singleton = false,
+      .is_a_reference = false,
+    },
+    .key_hash = key_hash,
+    .value_hash = value_hash,
+  };
+  return true;
+
+VALUE_HASH_CREATE_FAILED:
+  GTA_HASHX_DESTROY(key_hash);
+KEY_HASH_CREATE_FAILED:
+  return false;
+}
+
+
+void GTA_CALL gta_computed_value_map_destroy(GTA_Computed_Value * self) {
+  gta_computed_value_map_destroy_in_place(self);
+  gcu_free(self);
+}
+
+
+void GTA_CALL gta_computed_value_map_destroy_in_place(GTA_Computed_Value * self) {
+  GTA_Computed_Value_Map * map = (GTA_Computed_Value_Map *)self;
+  GTA_HASHX_DESTROY(map->key_hash);
+  GTA_HASHX_DESTROY(map->value_hash);
+}
+
+
+GTA_Computed_Value * GTA_CALL gta_computed_value_map_deep_copy(GTA_Computed_Value * value, GTA_Execution_Context * context) {
+  GTA_Computed_Value_Map * map = (GTA_Computed_Value_Map *)value;
+
+  // Create a new map into which values can be copied.
+  GTA_Computed_Value_Map * map_copy = (GTA_Computed_Value_Map *)gta_computed_value_map_create(map->key_hash->entries, context);
+  if (!map_copy || !GTA_COMPUTED_VALUE_IS_MAP(map_copy)) {
+    return gta_computed_value_error_out_of_memory;
+  }
+
+  // Copy the keys and values from the original map to the new map.
+  GTA_HashX_Iterator key_iterator = GTA_HASHX_ITERATOR_GET(map->key_hash);
+  GTA_HashX_Iterator value_iterator = GTA_HASHX_ITERATOR_GET(map->value_hash);
+
+  while (key_iterator.exists && value_iterator.exists) {
+    GTA_Computed_Value * key_copy = gta_computed_value_deep_copy((GTA_Computed_Value *)GTA_TYPEX_P(key_iterator.value), context);
+    GTA_Computed_Value * value_copy = gta_computed_value_deep_copy((GTA_Computed_Value *)GTA_TYPEX_P(value_iterator.value), context);
+
+    if (!key_copy
+      || !value_copy
+      || !GTA_HASHX_SET(map_copy->key_hash, key_iterator.hash, GTA_TYPEX_MAKE_P(key_copy))
+      || !GTA_HASHX_SET(map_copy->value_hash, value_iterator.hash, GTA_TYPEX_MAKE_P(value_copy))) {
+      gta_computed_value_map_destroy((GTA_Computed_Value *)map_copy);
+      return gta_computed_value_error_out_of_memory;
+    }
+
+    // Make sure that the key and value objects are not marked temporary.
+    key_copy->is_temporary = false;
+    value_copy->is_temporary = false;
+
+    // Advance the iterators.
+    key_iterator = GTA_HASHX_ITERATOR_NEXT(key_iterator);
+    value_iterator = GTA_HASHX_ITERATOR_NEXT(value_iterator);
+  }
+
+  // Sanity check: make sure that the hash tables have the same number of entries.
+  if ((map->key_hash->entries != map_copy->key_hash->entries)
+    || (map->value_hash->entries != map_copy->value_hash->entries)) {
+    gta_computed_value_map_destroy((GTA_Computed_Value *)map_copy);
+    return gta_computed_value_error_out_of_memory;
+  }
+
+  return (GTA_Computed_Value *)map_copy;
+}
+
+
+/*
+ * Helper function to expand and add to the provided string.
+ */
+static bool expand_and_concat(char * * buffer, size_t * len, const char * string) {
+  size_t string_length = strlen(string);
+  char * new_buffer = gcu_realloc(*buffer, *len + string_length + 1);
+  if (!new_buffer) {
+    return false;
+  }
+  strcpy(new_buffer + *len, string);
+  return new_buffer;
+}
+
+
+char * GTA_CALL gta_computed_value_map_to_string(GTA_Computed_Value * self) {
+  GTA_Computed_Value_Map * map = (GTA_Computed_Value_Map *)self;
+  char * buffer = gcu_malloc(3);
+  if (!buffer) {
+    return NULL;
+  }
+  buffer[0] = '{';
+  buffer[1] = '}';
+  buffer[2] = '\0';
+  size_t len = 0;
+  if (map->key_hash->entries == 0) {
+    return buffer;
+  }
+  buffer[1] = '\n';
+  
+  GTA_HashX_Iterator key_iterator = GTA_HASHX_ITERATOR_GET(map->key_hash);
+  GTA_HashX_Iterator value_iterator = GTA_HASHX_ITERATOR_GET(map->value_hash);
+  while (key_iterator.exists && value_iterator.exists) {
+    char * key_str = gta_computed_value_to_string((GTA_Computed_Value *)GTA_TYPEX_P(key_iterator.value));
+    if (!key_str) {
+      gcu_free(buffer);
+      return NULL;
+    }
+    char * value_str = gta_computed_value_to_string((GTA_Computed_Value *)GTA_TYPEX_P(value_iterator.value));
+    if (!value_str) {
+      gcu_free(key_str);
+      gcu_free(buffer);
+      return NULL;
+    }
+    if (!expand_and_concat(&buffer, &len, "  \"")
+      || !expand_and_concat(&buffer, &len, key_str)
+      || !expand_and_concat(&buffer, &len, "\": ")
+      || !expand_and_concat(&buffer, &len, value_str)
+      || !expand_and_concat(&buffer, &len, ",\n")) {
+      gcu_free(key_str);
+      gcu_free(value_str);
+      gcu_free(buffer);
+      return NULL;
+    }
+    gcu_free(key_str);
+    gcu_free(value_str);
+    key_iterator = GTA_HASHX_ITERATOR_NEXT(key_iterator);
+    value_iterator = GTA_HASHX_ITERATOR_NEXT(value_iterator);
+  }
+  if (!expand_and_concat(&buffer, &len, "}")) {
+    gcu_free(buffer);
+    return NULL;
+  }
+  return buffer;
+}
+
+
+GTA_Computed_Value * GTA_CALL gta_computed_value_map_get_from_cstring(GTA_Computed_Value_Map * self, const char * key) {
+  GTA_HashX_Value result = GTA_HASHX_GET(self->value_hash, gcu_string_hash_64(key, strlen(key)));
+  if (!result.exists) {
+    return gta_computed_value_error_map_key_not_found;
+  }
+  return (GTA_Computed_Value *)GTA_TYPEX_P(result.value);
+}
+
+
+GTA_Computed_Value * GTA_CALL gta_computed_value_map_set_key_val(GTA_Computed_Value_Map * self, GTA_Computed_Value * key, GTA_Computed_Value * value) {
+  if (!GTA_COMPUTED_VALUE_IS_STRING(key)) {
+    return gta_computed_value_error_map_key_not_string;
+  }
+
+  // Compute the hash of the key.
+  GTA_Computed_Value_String * key_string = (GTA_Computed_Value_String *)key;
+  GTA_Integer key_hash = gcu_string_hash_64(key_string->value->buffer, key_string->value->byte_length);
+
+  // Insert the key and value into the hash tables.
+  if (!GTA_HASHX_SET(self->key_hash, key_hash, GTA_TYPEX_MAKE_P(key))
+    || !GTA_HASHX_SET(self->value_hash, key_hash, GTA_TYPEX_MAKE_P(value))) {
+    return gta_computed_value_error_out_of_memory;
+  }
+
+  // Claim ownership of the key and value.
+  key->is_temporary = false;
+  value->is_temporary = false;
+
+  return value;
+}
