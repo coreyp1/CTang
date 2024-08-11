eb909e4dd18539e216cf4e4b71fd9b9612af64c7
Corey Pennycuff <pennycuff.c@gmail.com>
2024-08-11 00:56:56 -0500

    Code hardening: add asserts to astNodeIndex.c

--- src/ast/astNodeIndex.c
+++ src/ast/astNodeIndex.c
@@ -1,4 +1,5 @@
 
+#include <assert.h>
 #include <stdio.h>
 #include <string.h>
 #include <cutil/memory.h>
@@ -21,10 +22,14 @@ GTA_Ast_Node_VTable gta_ast_node_index_vtable = {
 
 
 GTA_Ast_Node_Index * gta_ast_node_index_create(GTA_Ast_Node * lhs, GTA_Ast_Node * rhs, GTA_PARSER_LTYPE location) {
+  assert(lhs);
+  assert(rhs);
+
   GTA_Ast_Node_Index * self = gcu_malloc(sizeof(GTA_Ast_Node_Index));
   if (!self) {
     return 0;
   }
+
   *self = (GTA_Ast_Node_Index) {
     .base = {
       .vtable = &gta_ast_node_index_vtable,
@@ -40,7 +45,10 @@ GTA_Ast_Node_Index * gta_ast_node_index_create(GTA_Ast_Node * lhs, GTA_Ast_Node 
 
 
 void gta_ast_node_index_destroy(GTA_Ast_Node * self) {
+  assert(self);
+  assert(GTA_AST_IS_INDEX(self));
   GTA_Ast_Node_Index * index = (GTA_Ast_Node_Index *) self;
+
   gta_ast_node_destroy(index->lhs);
   gta_ast_node_destroy(index->rhs);
   gcu_free(self);
@@ -48,7 +56,11 @@ void gta_ast_node_index_destroy(GTA_Ast_Node * self) {
 
 
 void gta_ast_node_index_print(GTA_Ast_Node * self, const char * indent) {
+  assert(self);
+  assert(GTA_AST_IS_INDEX(self));
   GTA_Ast_Node_Index * index = (GTA_Ast_Node_Index *) self;
+
+  assert(indent);
   char * new_indent = gcu_malloc(strlen(indent) + 5);
   if (!new_indent) {
     return;
@@ -56,9 +68,14 @@ void gta_ast_node_index_print(GTA_Ast_Node * self, const char * indent) {
   size_t indent_len = strlen(indent);
   memcpy(new_indent, indent, indent_len + 1);
   memcpy(new_indent + indent_len, "    ", 5);
+
+  assert(self->vtable);
+  assert(self->vtable->name);
   printf("%s%s\n", indent, self->vtable->name);
+
   printf("%s  LHS:\n", indent);
   gta_ast_node_print(index->lhs, new_indent);
+
   printf("%s  RHS:\n", indent);
   gta_ast_node_print(index->rhs, new_indent);
   gcu_free(new_indent);
@@ -66,12 +83,16 @@ void gta_ast_node_index_print(GTA_Ast_Node * self, const char * indent) {
 
 
 GTA_Ast_Node * gta_ast_node_index_simplify(GTA_Ast_Node * self, GTA_Ast_Simplify_Variable_Map * variable_map) {
+  assert(self);
+  assert(GTA_AST_IS_INDEX(self));
   GTA_Ast_Node_Index * index = (GTA_Ast_Node_Index *) self;
+
   GTA_Ast_Node * simplified_lhs = gta_ast_node_simplify(index->lhs, variable_map);
   if (simplified_lhs) {
     gta_ast_node_destroy(index->lhs);
     index->lhs = simplified_lhs;
   }
+
   GTA_Ast_Node * simplified_rhs = gta_ast_node_simplify(index->rhs, variable_map);
   if (simplified_rhs) {
     gta_ast_node_destroy(index->rhs);
@@ -82,15 +103,22 @@ GTA_Ast_Node * gta_ast_node_index_simplify(GTA_Ast_Node * self, GTA_Ast_Simplify
 
 
 void gta_ast_node_index_walk(GTA_Ast_Node * self, GTA_Ast_Node_Walk_Callback callback, void * data, void * return_value) {
-  callback(self, data, return_value);
+  assert(self);
+  assert(GTA_AST_IS_INDEX(self));
   GTA_Ast_Node_Index * index = (GTA_Ast_Node_Index *) self;
+
+  callback(self, data, return_value);
+
   gta_ast_node_walk(index->lhs, callback, data, return_value);
   gta_ast_node_walk(index->rhs, callback, data, return_value);
 }
 
 
 GTA_Ast_Node * gta_ast_node_index_analyze(GTA_Ast_Node * self, GTA_Program * program, GTA_Variable_Scope * scope) {
+  assert(self);
+  assert(GTA_AST_IS_INDEX(self));
   GTA_Ast_Node_Index * index = (GTA_Ast_Node_Index *) self;
+
   GTA_Ast_Node * error = gta_ast_node_analyze(index->lhs, program, scope);
   return error
     ? error
@@ -99,7 +127,14 @@ GTA_Ast_Node * gta_ast_node_index_analyze(GTA_Ast_Node * self, GTA_Program * pro
 
 
 bool gta_ast_node_index_compile_to_bytecode(GTA_Ast_Node * self, GTA_Compiler_Context * context) {
+  assert(self);
+  assert(GTA_AST_IS_INDEX(self));
   GTA_Ast_Node_Index * index = (GTA_Ast_Node_Index *) self;
+
+  assert(context);
+  assert(context->program);
+  assert(context->program->bytecode);
+  assert(context->bytecode_offsets);
   return gta_ast_node_compile_to_bytecode(index->lhs, context)
     && gta_ast_node_compile_to_bytecode(index->rhs, context)
       && GTA_BYTECODE_APPEND(context->bytecode_offsets, context->program->bytecode->count)
@@ -108,7 +143,12 @@ bool gta_ast_node_index_compile_to_bytecode(GTA_Ast_Node * self, GTA_Compiler_Co
 
 
 bool gta_ast_node_index_compile_to_binary__x86_64(GTA_Ast_Node * self, GTA_Compiler_Context * context) {
+  assert(self);
+  assert(GTA_AST_IS_INDEX(self));
   GTA_Ast_Node_Index * index = (GTA_Ast_Node_Index *) self;
+
+  assert(context);
+  assert(context->binary_vector);
   GCU_Vector8 * v = context->binary_vector;
 
   // Compile the expression.
