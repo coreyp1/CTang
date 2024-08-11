0172e72a740c6853608475890efd6e324b3539c9
Corey Pennycuff <pennycuff.c@gmail.com>
2024-08-11 00:14:12 -0500

    Code hardening: add asserts to astNodeBlock.c

--- src/ast/astNodeBlock.c
+++ src/ast/astNodeBlock.c
@@ -1,4 +1,5 @@
 
+#include <assert.h>
 #include <stdio.h>
 #include <string.h>
 #include <cutil/memory.h>
@@ -23,10 +24,13 @@ GTA_Ast_Node_VTable gta_ast_node_block_vtable = {
 
 
 GTA_Ast_Node_Block * gta_ast_node_block_create(GTA_VectorX * statements, GTA_PARSER_LTYPE location) {
+  assert(statements);
+
   GTA_Ast_Node_Block * self = gcu_malloc(sizeof(GTA_Ast_Node_Block));
   if (!self) {
     return 0;
   }
+
   *self = (GTA_Ast_Node_Block) {
     .base = {
       .vtable = &gta_ast_node_block_vtable,
@@ -41,13 +45,21 @@ GTA_Ast_Node_Block * gta_ast_node_block_create(GTA_VectorX * statements, GTA_PAR
 
 
 void gta_ast_node_block_destroy(GTA_Ast_Node * self) {
+  assert(self);
+  assert(GTA_AST_IS_BLOCK(self));
   GTA_Ast_Node_Block * block = (GTA_Ast_Node_Block *) self;
+
   GTA_VECTORX_DESTROY(block->statements);
   gcu_free(self);
 }
 
 
 void gta_ast_node_block_print(GTA_Ast_Node * self, const char * indent) {
+  assert(self);
+  assert(GTA_AST_IS_BLOCK(self));
+  GTA_Ast_Node_Block * block = (GTA_Ast_Node_Block *) self;
+
+  assert(indent);
   size_t indent_len = strlen(indent);
   char * new_indent = gcu_malloc(indent_len + 3);
   if (!new_indent) {
@@ -55,8 +67,13 @@ void gta_ast_node_block_print(GTA_Ast_Node * self, const char * indent) {
   }
   memcpy(new_indent, indent, indent_len + 1);
   memcpy(new_indent + indent_len, "  ", 3);
+
+  assert(self->vtable);
+  assert(self->vtable->name);
   printf("%s%s:\n", indent, self->vtable->name);
-  GTA_Ast_Node_Block * block = (GTA_Ast_Node_Block *) self;
+
+  assert(block->statements);
+  assert(block->statements->count ? (bool)block->statements->data : true);
   for (size_t i = 0; i < GTA_VECTORX_COUNT(block->statements); ++i) {
     GTA_Ast_Node * statement = (GTA_Ast_Node *)GTA_TYPEX_P(block->statements->data[i]);
     gta_ast_node_print(statement, new_indent);
@@ -66,7 +83,12 @@ void gta_ast_node_block_print(GTA_Ast_Node * self, const char * indent) {
 
 
 GTA_Ast_Node * gta_ast_node_block_simplify(GTA_Ast_Node * self, GTA_Ast_Simplify_Variable_Map * variable_map) {
+  assert(self);
+  assert(GTA_AST_IS_BLOCK(self));
   GTA_Ast_Node_Block * block = (GTA_Ast_Node_Block *) self;
+
+  assert(block->statements);
+  assert(block->statements->count ? (bool)block->statements->data : true);
   for (size_t i = 0; i < GTA_VECTORX_COUNT(block->statements); ++i) {
     GTA_Ast_Node * simplified_statement = gta_ast_node_simplify((GTA_Ast_Node *)GTA_TYPEX_P(block->statements->data[i]), variable_map);
     if (simplified_statement) {
@@ -79,7 +101,12 @@ GTA_Ast_Node * gta_ast_node_block_simplify(GTA_Ast_Node * self, GTA_Ast_Simplify
 
 
 GTA_Ast_Node * gta_ast_node_block_analyze(GTA_Ast_Node * self, GTA_Program * program, GTA_Variable_Scope * scope) {
+  assert(self);
+  assert(GTA_AST_IS_BLOCK(self));
   GTA_Ast_Node_Block * block = (GTA_Ast_Node_Block *) self;
+
+  assert(block->statements);
+  assert(block->statements->count ? (bool)block->statements->data : true);
   for (size_t i = 0; i < GTA_VECTORX_COUNT(block->statements); ++i) {
     GTA_Ast_Node * statement = (GTA_Ast_Node *)GTA_TYPEX_P(block->statements->data[i]);
     GTA_Ast_Node * error = gta_ast_node_analyze(statement, program, scope);
@@ -92,8 +119,14 @@ GTA_Ast_Node * gta_ast_node_block_analyze(GTA_Ast_Node * self, GTA_Program * pro
 
 
 void gta_ast_node_block_walk(GTA_Ast_Node * self, GTA_Ast_Node_Walk_Callback callback, void * data, void * return_value) {
-  callback(self, data, return_value);
+  assert(self);
+  assert(GTA_AST_IS_BLOCK(self));
   GTA_Ast_Node_Block * block = (GTA_Ast_Node_Block *) self;
+
+  callback(self, data, return_value);
+
+  assert(block->statements);
+  assert(block->statements->count ? (bool)block->statements->data : true);
   for (size_t i = 0; i < GTA_VECTORX_COUNT(block->statements); ++i) {
     GTA_Ast_Node * statement = (GTA_Ast_Node *)GTA_TYPEX_P(block->statements->data[i]);
     gta_ast_node_walk(statement, callback, data, return_value);
@@ -102,9 +135,12 @@ void gta_ast_node_block_walk(GTA_Ast_Node * self, GTA_Ast_Node_Walk_Callback cal
 
 
 bool gta_ast_node_block_compile_to_bytecode(GTA_Ast_Node * self, GTA_Compiler_Context * context) {
+  assert(self);
+  assert(GTA_AST_IS_BLOCK(self));
   GTA_Ast_Node_Block * block = (GTA_Ast_Node_Block *) self;
 
   // If the block has no statements, then put a NULL on the stack.
+  assert(block->statements);
   if (!GTA_VECTORX_COUNT(block->statements)) {
     return GTA_BYTECODE_APPEND(context->bytecode_offsets, context->program->bytecode->count)
       && GTA_VECTORX_APPEND(context->program->bytecode, GTA_TYPEX_MAKE_UI(GTA_BYTECODE_NULL));
@@ -116,6 +152,7 @@ bool gta_ast_node_block_compile_to_bytecode(GTA_Ast_Node * self, GTA_Compiler_Co
   // Function declarations are not an executed statement, so we don't need to
   // POP after compiling a function declaration.
   bool statements_compiled = false;
+  assert(block->statements->count ? (bool)block->statements->data : true);
   for (size_t i = 0; i < GTA_VECTORX_COUNT(block->statements); ++i) {
     GTA_Ast_Node * statement = (GTA_Ast_Node *)GTA_TYPEX_P(block->statements->data[i]);
     if (!GTA_AST_IS_USE(statement)) {
@@ -132,6 +169,7 @@ bool gta_ast_node_block_compile_to_bytecode(GTA_Ast_Node * self, GTA_Compiler_Co
       }
     }
   }
+
   if (statements_compiled) {
     // The last statement should be left on the stack, so remove the last pop.
     --context->bytecode_offsets->count;
@@ -142,8 +180,12 @@ bool gta_ast_node_block_compile_to_bytecode(GTA_Ast_Node * self, GTA_Compiler_Co
 
 
 bool gta_ast_node_block_compile_to_binary__x86_64(GTA_Ast_Node * self, GTA_Compiler_Context * context) {
+  assert(self);
+  assert(GTA_AST_IS_BLOCK(self));
   GTA_Ast_Node_Block * block = (GTA_Ast_Node_Block *) self;
 
+  assert(block->statements);
+  assert(block->statements->count ? (bool)block->statements->data : true);
   for (size_t i = 0; i < GTA_VECTORX_COUNT(block->statements); ++i) {
     GTA_Ast_Node * statement = (GTA_Ast_Node *)GTA_TYPEX_P(block->statements->data[i]);
     if (!GTA_AST_IS_USE(statement)) {
