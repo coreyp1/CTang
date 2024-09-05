007a812aae0f0ba2fa5561ce5e2e2380a8e4bd0e
Corey Pennycuff <pennycuff.c@gmail.com>
2024-09-04 23:03:48 -0500

    Fleshing out the Library object before beginning to convert old code to this new format.

--- include/tang/library/library.h
+++ include/tang/library/library.h
@@ -13,6 +13,21 @@ extern "C" {
 #include <tang/macros.h>
 
 /**
+ * Library callback declaration type.
+ *
+ * Not all libraries that are available will be used by any particular script.
+ * In order to prevent unnecessary memory allocations, libraries will not be
+ * instantiated unless they are actually invoked in the script.  When
+ * registering a global using gta_library_add_library(), a callback with this
+ * signature must be provided.  This callback will be invoked during the script
+ * execution.
+ *
+ * @param context The context of the currently executing script.
+ * @return The resulting computed value.
+ */
+typedef GTA_Computed_Value * GTA_CALL (*GTA_Library_Callback) (GTA_Execution_Context * context);
+
+/**
  * The library structure.
  */
 typedef struct GTA_Library {
@@ -35,6 +50,26 @@ GTA_NO_DISCARD GTA_Library * gta_library_create(void);
  * @param library The library object to destroy.
  */
 void gta_library_destroy(GTA_Library * library);
+
+/**
+ * Adds a library entry to the execution context.
+ *
+ * @param context The execution context.
+ * @param hash The hash of the library entry's identifier.
+ * @param func The function to be invoked to create the Computed Value.
+ * @return true on success, false on failure.
+ */
+bool gta_library_add_library(GTA_Library * library, GTA_UInteger hash, GTA_Library_Callback func);
+
+/**
+ * Get the library entry for the given hash.
+ *
+ * @param library The library object.
+ * @param hash The hash of the library entry identifier.
+ * @return GTA_HashX_Value 
+ */
+GTA_HashX_Value gta_library_get_library(GTA_Library * library, GTA_UInteger hash);
+
 
 #ifdef __cplusplus
 }
--- src/library/library.c
+++ src/library/library.c
@@ -4,6 +4,16 @@
 #include <cutil/memory.h>
 #include <tang/library/library.h>
 
+/**
+ * Helper class, only existing to aid in the transform a function pointer to a
+ * value able to be stored in the hash.
+ */
+typedef union GTA_Library_Callback_Function_Converter {
+  GTA_Library_Callback f;
+  void * b;
+} GTA_Library_Callback_Function_Converter;
+
+
 GTA_Library * gta_library_create(void) {
   GTA_Library * library = gcu_malloc(sizeof(GTA_Library));
   if (library == NULL) {
@@ -21,10 +31,25 @@ LIBRARY_CREATE_FAILED:
   return NULL;
 }
 
+
 void gta_library_destroy(GTA_Library * library) {
   assert(library);
   assert(library->manifest);
 
   GTA_HASHX_DESTROY(library->manifest);
   gcu_free(library);
+}
+
+
+bool gta_library_add_library(GTA_Library * library, GTA_UInteger hash, GTA_Library_Callback func) {
+  assert(library);
+  assert(library->manifest);
+  return GTA_HASHX_SET(library->manifest, hash, GTA_TYPEX_MAKE_P((GTA_Library_Callback_Function_Converter){.f = func}.b));
+}
+
+
+GTA_HashX_Value gta_library_get_library(GTA_Library * library, GTA_UInteger hash) {
+  assert(library);
+  assert(library->manifest);
+  return GTA_HASHX_GET(library->manifest, hash);
 }
